04058be6f460a4bde5c24a3a4605890bd149919a
NATHALIA MINOSSI OLIVEIRA <04987029014@senacrs.edu.br>
2025-09-04 17:20:09 -0300

    atualizado

--- 25-08/src/server.ts
+++ 25-08/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import userRoutes from "./routes";
+import { AppDataSource } from "./data-source";
 
 const app: Application = express();
 const PORT: number = 3000;
@@ -8,6 +9,14 @@ app.use(express.json()); // DEFINE QUE MINHA API TRABALHA COM JSON
 
 app.use(userRoutes); //QUERO UTILIZAR MINHAS ROTAS
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-})
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Banco de dados conectado com sucesso!");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Erro ao conectar ao banco de dados:", err);
+  });
